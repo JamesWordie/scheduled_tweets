52ba6c56e48f16644541e57d7213c7d8edc57d9f
James Wordie <james.wordie@yahoo.com>
2021-06-17 16:59:49 +0100

    added bcrypt gem to gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
+# has two fields email and password_digest
+# bcrypt gives has_secure_password for encryption
+# gives 2 additional fields password and password_confirmation
+
 class User < ApplicationRecord
+  has_secure_password
 end
