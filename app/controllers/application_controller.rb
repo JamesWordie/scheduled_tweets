39b768e16e013c3e67a38e2461b2acd9e26d88b2
James Wordie <james.wordie@yahoo.com>
2021-06-17 18:49:32 +0100

    added the ability to update your password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
       Current.user = User.find_by(id: session[:user_id]) # use find_by so that it doesn't throw an error if no user_id exists
     end
   end
+
+  def require_user_logged_in!
+    redirect_to sign_in_path, alert: "You must be signed in to do that." if Current.user.nil?
+  end
 end
