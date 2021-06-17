404fe8194df82fd69170b61204011afb74e63682
James Wordie <james.wordie@yahoo.com>
2021-06-17 17:41:13 +0100

    added in form for sign up and error handling

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@
 
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true,
+                    format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'must be a valid email address' },
+                    uniqueness: true # presence validation add null false to migration
+  validates :password_digest, presence: true
 end
