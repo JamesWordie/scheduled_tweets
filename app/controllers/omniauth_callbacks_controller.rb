b0845e3c50e2843e4baeb47cb6e632a243af4f55
James Wordie <james.wordie@yahoo.com>
2021-06-19 17:58:51 +0100

    setup of omniauth controller for saving credentials

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,18 @@
 class OmniauthCallbacksController < ApplicationController
   def twitter
-    render plain: "success"
+    Rails.logger.info auth
+    # current_user same as devise has, setup earlier
+    Current.user.twitter_accounts.create(
+      name: auth.info.name,
+      username: auth.info.nickname,
+      image: auth.info.image,
+      token: auth.credentials.token,
+      secret: auth.credentials.secret
+    )
+  end
+
+  def auth
+    # standard way of grabbing the credentials as a hash, built in to omniauth ALL APIS STANDARD WAY TO OAUTH
+    request.env['omniauth.auth']
   end
 end
