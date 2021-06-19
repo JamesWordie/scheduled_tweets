e9046345eabd4af055c78abcbaef8a2a0a73e1d0
James Wordie <james.wordie@yahoo.com>
2021-06-19 18:35:58 +0100

    added in twitter account controller for index and destroy

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,13 +2,18 @@ class OmniauthCallbacksController < ApplicationController
   def twitter
     Rails.logger.info auth
     # current_user same as devise has, setup earlier
-    Current.user.twitter_accounts.create(
+    twitter_account = Current.user.twitter_accounts
+                             .where(username: auth.info.nickname)
+                             .first_or_initialize
+    twitter_account.update(
       name: auth.info.name,
       username: auth.info.nickname,
       image: auth.info.image,
       token: auth.credentials.token,
       secret: auth.credentials.secret
     )
+
+    redirect_to twitter_accounts_path, notice: "Successfully connected your account"
   end
 
   def auth
