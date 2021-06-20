9f923b1f8ce4d8d2d7bc68385039b10ce7670637
James Wordie <james.wordie@yahoo.com>
2021-06-20 08:27:47 +0100

    setup of the partial for the new and edit forms and update and destroy actions with links

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :require_user_logged_in!
+  before_action :set_tweet, only: %i[edit update destroy]
 
   def index
     @tweets = Current.user.tweets
@@ -19,20 +20,28 @@ class TweetsController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
-
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "Updated the tweet."
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @tweet.destroy
+    redirect_to tweets_path, alert: "Tweet was sucessfully un-scheduled"
   end
 
   private
 
   def tweet_params
     params.require(:tweet).permit(:body, :publish_at, :twitter_account_id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
